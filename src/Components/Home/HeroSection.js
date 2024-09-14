8e657d4284c9bf7a66214f12d68a84af7200f28c
ranyon <kwakumensahj@gmail.com>
2024-09-14 18:13:34 +0000

    added gradient

--- src/Components/Home/HeroSection.js
+++ src/Components/Home/HeroSection.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import './Hero.css'
 
 const HeroContainer = styled(Container)`
-  height: 100vh;
+  height: 80vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
