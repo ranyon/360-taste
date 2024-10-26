e1c31b82f49fbe1123180b1f1c6df31503ca532f
ranyon <kwakumensahj@gmail.com>
2024-10-26 13:08:59 +0000

    fixed navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { useEffect } from 'react';
-import { Element } from 'react-scroll';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import NavbarComponent from './Components/Navbar/NavBarComponent';
 import HeroSection from './Components/Home/HeroSection';
 import Menu from './Components/Menu/Menu';
+import OrderPage from './Components/order/OrderPage';
 import Footer from './Components/Footer/Footer';
 import './App.css';
+
+function ScrollToTop() {
+  const { pathname, hash } = useLocation();
+
+  useEffect(() => {
+    if (hash === '') {
+      window.scrollTo(0, 0);
+    } else {
+      const element = document.getElementById(hash.slice(1));
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [pathname, hash]);
+
+  return null;
+}
 
 function App() {
   useEffect(() => {
@@ -17,31 +35,34 @@ function App() {
   }, []);
 
   return (
-    <div className="app-container">
-      <div className="content">
-        <NavbarComponent />
-        <Element name="home">
-          <div data-aos="fade-up">
-            <HeroSection />
-          </div>
-        </Element>
-        <Element name="menu">
-          <div data-aos="fade-up">
-            <Menu />
-          </div>
-        </Element>
-        <Element name="footer">
-          <div data-aos="fade-up">
+    <Router>
+      <ScrollToTop />
+      <div className="app-container">
+        <div className="content">
+          <NavbarComponent />
+          <Routes>
+            <Route path="/" element={
+              <>
+                <div data-aos="fade-up" id="home">
+                  <HeroSection />
+                </div>
+                <div data-aos="fade-up" id="menu">
+                  <Menu />
+                </div>
+              </>
+            } />
+            <Route path="/order" element={<OrderPage />} />
+            <Route path="/order/:categoryId" element={<OrderPage />} />
+          </Routes>
+          <div data-aos="fade-up" id="contact">
             <Footer />
           </div>
-        </Element>
+        </div>
+        <div className="background"></div>
+        <div className="overlay"></div>
       </div>
-      <div className="background"></div>
-      <div className="overlay"></div>
-    </div>
+    </Router>
   );
 }
 
 export default App;
-
-//STABLE VERSION
--- src/Components/Navbar/NavBarComponent.js
+++ src/Components/Navbar/NavBarComponent.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
-import { Link } from 'react-scroll';
 import Logo from '../images/360logo.png'
-
 
 const NavbarComponent = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
-      <Navbar.Brand as={Link} to="home" smooth={true} duration={500}>
-      <img
-          src={Logo}
-          alt="360 Taste Logo"
-          width="60"
-          height="60"
-        />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Link as={Link} to="home" smooth={true} duration={500}>Home</Nav.Link>
-          <Nav.Link as={Link} to="menu" smooth={true} duration={500}>Menu</Nav.Link>
-          <Nav.Link as={Link} to="footer" smooth={true} duration={500}>Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src={Logo}
+            alt="360 Taste Logo"
+            width="60"
+            height="60"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/#home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/#menu">Menu</Nav.Link>
+            <Nav.Link as={Link} to="/order">Order</Nav.Link>
+            <Nav.Link as={Link} to="/#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
 
 export default NavbarComponent;
-
-
-//STABLE VERSION
